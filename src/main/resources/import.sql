ddf36e7eb520b08421fa655cd465e5af34c96786
Valeria Mojica <mojicacastanedavaleria@gmail.com>
2022-08-15 15:33:47 -0500

    Agregar usuarios

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,36 +1,36 @@
-INSERT INTO customer_db.customer_order_products (order_order_id, products_product_id) VALUES (1,1);
-INSERT INTO customer_db.customer_order_products (order_order_id, products_product_id) VALUES (1,2);
-INSERT INTO customer_db.customer_order_products (order_order_id, products_product_id) VALUES (1,3);
-INSERT INTO customer_db.customer_order_products (order_order_id, products_product_id) VALUES (2,1);
-INSERT INTO customer_db.customer_order_products (order_order_id, products_product_id) VALUES (3,2);
-INSERT INTO customer_db.customer_order_products (order_order_id, products_product_id) VALUES (4,3);
-INSERT INTO customer_db.customer_order_products (order_order_id, products_product_id) VALUES (5,2);
-INSERT INTO customer_db.customer_order_products (order_order_id, products_product_id) VALUES (5,3);
+INSERT INTO customer_order_products (order_order_id, products_product_id) VALUES (1,1);
+INSERT INTO customer_order_products (order_order_id, products_product_id) VALUES (1,2);
+INSERT INTO customer_order_products (order_order_id, products_product_id) VALUES (1,3);
+INSERT INTO customer_order_products (order_order_id, products_product_id) VALUES (2,1);
+INSERT INTO customer_order_products (order_order_id, products_product_id) VALUES (3,2);
+INSERT INTO customer_order_products (order_order_id, products_product_id) VALUES (4,3);
+INSERT INTO customer_order_products (order_order_id, products_product_id) VALUES (5,2);
+INSERT INTO customer_order_products (order_order_id, products_product_id) VALUES (5,3);
 
 /* Se insertan los siguientes registros en la tabla */
 
-INSERT INTO customer_db.customer_data (first_name, last_name) VALUES('Benito', 'Bodoque');
-INSERT INTO customer_db.customer_data (first_name, last_name) VALUES('Reese', 'Wilkerson');
-INSERT INTO customer_db.customer_data (first_name, last_name) VALUES('Nelson', 'Montz');
-INSERT INTO customer_db.customer_data (first_name, last_name) VALUES('Leonel', 'Messi');
-INSERT INTO customer_db.customer_data (first_name, last_name) VALUES('Rick', 'Sánchez');
+INSERT INTO customer_data (first_name, last_name) VALUES('Benito', 'Bodoque');
+INSERT INTO customer_data (first_name, last_name) VALUES('Reese', 'Wilkerson');
+INSERT INTO customer_data (first_name, last_name) VALUES('Nelson', 'Montz');
+INSERT INTO customer_data (first_name, last_name) VALUES('Leonel', 'Messi');
+INSERT INTO customer_data (first_name, last_name) VALUES('Rick', 'Sánchez');
 
-INSERT INTO customer_db.customer_data (first_name, last_name) VALUES('Cristiano', 'Ronaldo');
-INSERT INTO customer_db.customer_data (first_name, last_name) VALUES('Lois', 'Wilkerson');
-INSERT INTO customer_db.customer_data (first_name, last_name) VALUES('Perlita', 'Cangrejo');
-INSERT INTO customer_db.customer_data (first_name, last_name) VALUES('Patricio', 'Estrella');
-INSERT INTO customer_db.customer_data (first_name, last_name) VALUES('Espergencia', 'Valdés');
+INSERT INTO customer_data (first_name, last_name) VALUES('Cristiano', 'Ronaldo');
+INSERT INTO customer_data (first_name, last_name) VALUES('Lois', 'Wilkerson');
+INSERT INTO customer_data (first_name, last_name) VALUES('Perlita', 'Cangrejo');
+INSERT INTO customer_data (first_name, last_name) VALUES('Patricio', 'Estrella');
+INSERT INTO customer_data (first_name, last_name) VALUES('Espergencia', 'Valdés');
 
-INSERT INTO customer_db.customer_data (first_name, last_name) VALUES('Edna', 'Krabappel');
-INSERT INTO customer_db.customer_data (first_name, last_name) VALUES('Lois', 'Wilkerson');
-INSERT INTO customer_db.customer_data (first_name, last_name) VALUES('Eric', 'Cartman');
-INSERT INTO customer_db.customer_data (first_name, last_name) VALUES('Kenny', 'McCormick');
-INSERT INTO customer_db.customer_data (first_name, last_name) VALUES('Thomas', 'Anderson');
+INSERT INTO customer_data (first_name, last_name) VALUES('Edna', 'Krabappel');
+INSERT INTO customer_data (first_name, last_name) VALUES('Lois', 'Wilkerson');
+INSERT INTO customer_data (first_name, last_name) VALUES('Eric', 'Cartman');
+INSERT INTO customer_data (first_name, last_name) VALUES('Kenny', 'McCormick');
+INSERT INTO customer_data (first_name, last_name) VALUES('Thomas', 'Anderson');
 
-INSERT INTO customer_db.customer_data (first_name, last_name) VALUES('Marty', 'McFly');
-INSERT INTO customer_db.customer_data (first_name, last_name) VALUES('Saul', 'Goodman');
-INSERT INTO customer_db.customer_data (first_name, last_name) VALUES('Federico', 'Bardón');
-INSERT INTO customer_db.customer_data (first_name, last_name) VALUES('Walter', 'White');
-INSERT INTO customer_db.customer_data (first_name, last_name) VALUES('Emmett', 'Brown');
+INSERT INTO customer_data (first_name, last_name) VALUES('Marty', 'McFly');
+INSERT INTO customer_data (first_name, last_name) VALUES('Saul', 'Goodman');
+INSERT INTO customer_data (first_name, last_name) VALUES('Federico', 'Bardón');
+INSERT INTO customer_data (first_name, last_name) VALUES('Walter', 'White');
+INSERT INTO customer_data (first_name, last_name) VALUES('Emmett', 'Brown');
 
 
